4eaf7de3f961cdbda2ca84570e6365924cd49410
Antonio Wilson <antonio@ncstore.biz>
2022-09-08 18:24:30 -0400

    Updated spaces to tabs in html files, updated to use form fields rather than hard coded form results

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)  
 
@@ -8,7 +9,11 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    count = int(request.form["strawberry"]) + int(request.form["raspberry"]) + int(request.form["apple"])
+    date = datetime.now().strftime("%B %d, %Y %I:%M:%S %p")
+    customer = f"{request.form['first_name']} {request.form['last_name']}"
+    print(f"Charging {customer} for {count} fruits")
+    return render_template("checkout.html", count = count, date = date)
 
 @app.route('/fruits')         
 def fruits():
